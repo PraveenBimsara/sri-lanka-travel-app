b8648cef785e696d595a52c909e6f51ec19900c3
PraveenBimsara <praveenbimsara99@gmail.com>
2024-06-17 13:20:17 +0530

    Created explore stories section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import AboutUs from './components/AboutUs/AboutUs'
+import Explore from './components/Explore/Explore'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <AboutUs/>
+      <Explore/>
     </div>
   )
 }
---
+++ src/components/Explore/Explore.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Explore.css";
+import explore from '../../assets/explore.jpg'
+
+const Explore = () => {
+  return (
+    <div className="explore">
+      <div className="explore-content">
+        <div className="explore-title">
+          <h1>Explore Island Stories</h1>
+          <p>
+            Each place, and each smile in Sri Lanka has a story to tell.
+            <br />
+            We have so much to share with you, so come along to our
+            <br />
+            island in paradise!
+          </p>
+        </div>
+        <div className="explore-button">EXPLORE MORE</div>
+      </div>
+    </div>
+  );
+};
+
+export default Explore;
