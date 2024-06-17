9b568d28d033436c0fa51c97c75f25c04f5482bf
PraveenBimsara <praveenbimsara99@gmail.com>
2024-06-17 20:09:26 +0530

    Finished new products section

--- src/components/NewProducts/NewProducts.jsx
+++ src/components/NewProducts/NewProducts.jsx
@@ -3,9 +3,14 @@ import './NewProducts.css'
 import ramayana from '../../assets/Ramayana.jpg'
 import buddhist from '../../assets/Buddhist-place.jpg'
 import welness from '../../assets/Welness.jpg'
+import wilpaththu from'../../assets/Wilpaththu.jpg'
+import endfinal from '../../assets/end-final.png'
+import sltpb from '../../assets/SLTPB.jpg'
+import promotion from '../../assets/Promotional.jpg'
 
 const NewProducts = () => {
   return (
+    <>
     <div className='new-products'>
       <div className="new-products-title">
         <h1>New Products</h1>
@@ -34,6 +39,41 @@ const NewProducts = () => {
         </div>
       </div>
     </div>
+    <hr className='new-hr'/>
+
+    <div className='new-products'>
+      <div className="new-products-title">
+        <h1>What's New</h1>
+      </div>
+      <div className="products-container">
+        <div className="new-product">
+          <div className="image-container">
+            <img className='new-image' src={wilpaththu} alt=''/>
+            <h1 className='new-h'>Wilpattu National<br/>Park Wildlife<br/>Streaming</h1>
+          </div>
+        </div>
+        <div className="new-product">
+          <div className="image-container">
+            <img className='new-image' src={endfinal} alt=''/>
+            <h1 className='new-h'>International<br/>Endorsement</h1>
+          </div>
+        </div>
+        <div className="new-product">
+          <div className="image-container">
+            <img className='new-image' src={sltpb} alt=''/>
+            <h1 className='new-h'>Greatest Shows<br/>on Earth</h1>
+          </div>
+        </div>
+        <div className="new-product">
+          <div className="image-container">
+            <img className='new-image' src={promotion} alt=''/>
+            <h1 className='new-h'>SLTPB<br/>Promotional KIT</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+</>
+    
   )
 }
 
